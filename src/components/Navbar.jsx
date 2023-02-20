0b0c1cc858b1ea118366ae04bae506f33e11493a
TolgaT <tolga.topal@stud.fh-campuswien.ac.at>
2023-02-20 01:14:07 +0100

    new hover effect

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,14 +12,26 @@ const Navbar = () => {
   return (
     <div className="flex items-center justify-between h-24 px-4 max-w-[1600px] mx-auto text-white ">
       <img src={logo} alt="Profile Logo" width={40} className="mr-1"></img>
-      <h1 className="w-full text-4xl font-bold">Tolga Topal</h1>
+      <h1 className="w-full text-3xl font-bold md:text-4xl">Tolga Topal</h1>
       <ul className="hidden md:flex whitespace-nowrap">
-        <li className="p-4 cursor-pointer hover:font-bold">Home</li>
-        <li className="p-4 cursor-pointer hover:font-bold">About Me</li>
-        <li className="p-4 cursor-pointer hover:font-bold">Projects</li>
-        <li className="p-4 cursor-pointer hover:font-bold">Skills</li>
-        <li className="p-4 cursor-pointer hover:font-bold">Expierence</li>
-        <li className="p-4 cursor-pointer hover:font-bold">Contact Me</li>
+        <li className="p-4 cursor-pointer hover:font-bold hover:text-blue-400">
+          Home
+        </li>
+        <li className="p-4 cursor-pointer hover:font-bold hover:text-blue-400">
+          About Me
+        </li>
+        <li className="p-4 cursor-pointer hover:font-bold hover:text-blue-400">
+          Projects
+        </li>
+        <li className="p-4 cursor-pointer hover:font-bold hover:text-blue-400">
+          Skills
+        </li>
+        <li className="p-4 cursor-pointer hover:font-bold hover:text-blue-400">
+          Expierence
+        </li>
+        <li className="p-4 cursor-pointer hover:font-bold hover:text-blue-400">
+          Contact Me
+        </li>
       </ul>
       <div className="block cursor-pointer md:hidden" onClick={handleNav}>
         {!nav ? <AiOutlineClose size={25} /> : <AiOutlineMenu size={25} />}
@@ -28,25 +40,29 @@ const Navbar = () => {
         className={
           !nav
             ? "fixed left-0 top-0 w-[60%] border-r border-r-gray-900 h-full ease-in-out duration-500 "
-            : "fixed left-[-100%] ease-in-out duration-500"
+            : "fixed left-[-100%]"
         }
       >
         <div className="flex ml-4 mt-7">
-          <img src={logo} alt="Profile Logo" width={40} className="mr-1"></img>
-          <h1 className="w-full text-4xl font-bold">Tolga Topal</h1>
+          <img src={logo} alt="Profile Logo" width={35} className="mr-1"></img>
+          <h1 className="w-full text-3xl font-bold whitespace-nowrap">
+            Tolga Topal
+          </h1>
         </div>
         <ul className="uppercase ">
-          <li className="p-4 border-b cursor-pointer hover:font-bold">Home</li>
-          <li className="p-4 border-b cursor-pointer hover:font-bold">
+          <li className="p-4 border-b cursor-pointer hover:font-bold hover:text-blue-400">
+            Home
+          </li>
+          <li className="p-4 border-b cursor-pointer hover:font-bold hover:text-blue-400">
             About Me
           </li>
-          <li className="p-4 border-b cursor-pointer hover:font-bold">
+          <li className="p-4 border-b cursor-pointer hover:font-bold hover:text-blue-400">
             Projects
           </li>
-          <li className="p-4 border-b cursor-pointer hover:font-bold">
+          <li className="p-4 border-b cursor-pointer hover:font-bold hover:text-blue-400">
             Skills
           </li>
-          <li className="p-4 border-b cursor-pointer hover:font-bold">
+          <li className="p-4 border-b cursor-pointer hover:font-bold hover:text-blue-400">
             Expierence
           </li>
           <li className="p-4 cursor-pointer hover:font-bold">Contact Me</li>
