f8c91e3ae12155a5c69dc62ee5fe78ffc2677592
TolgaT <tolga.topal@stud.fh-campuswien.ac.at>
2023-02-20 01:02:16 +0100

    nav bar almost done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,59 @@
-import React from 'react'
+import React, { useState } from "react";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+import logo from "../components/favicon.png";
 
-export const Navbar = () => {
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
   return (
-    <div className="text-black">
-        <h1 className='w-full text-3xl font-bold'>REACT.</h1>
-        <ul>
-          <li></li>
-          <li></li>
-          <li></li>
+    <div className="flex items-center justify-between h-24 px-4 max-w-[1600px] mx-auto text-white ">
+      <img src={logo} alt="Profile Logo" width={40} className="mr-1"></img>
+      <h1 className="w-full text-4xl font-bold">Tolga Topal</h1>
+      <ul className="hidden md:flex whitespace-nowrap">
+        <li className="p-4 cursor-pointer hover:font-bold">Home</li>
+        <li className="p-4 cursor-pointer hover:font-bold">About Me</li>
+        <li className="p-4 cursor-pointer hover:font-bold">Projects</li>
+        <li className="p-4 cursor-pointer hover:font-bold">Skills</li>
+        <li className="p-4 cursor-pointer hover:font-bold">Expierence</li>
+        <li className="p-4 cursor-pointer hover:font-bold">Contact Me</li>
+      </ul>
+      <div className="block cursor-pointer md:hidden" onClick={handleNav}>
+        {!nav ? <AiOutlineClose size={25} /> : <AiOutlineMenu size={25} />}
+      </div>
+      <div
+        className={
+          !nav
+            ? "fixed left-0 top-0 w-[60%] border-r border-r-gray-900 h-full ease-in-out duration-500 "
+            : "fixed left-[-100%] ease-in-out duration-500"
+        }
+      >
+        <div className="flex ml-4 mt-7">
+          <img src={logo} alt="Profile Logo" width={40} className="mr-1"></img>
+          <h1 className="w-full text-4xl font-bold">Tolga Topal</h1>
+        </div>
+        <ul className="uppercase ">
+          <li className="p-4 border-b cursor-pointer hover:font-bold">Home</li>
+          <li className="p-4 border-b cursor-pointer hover:font-bold">
+            About Me
+          </li>
+          <li className="p-4 border-b cursor-pointer hover:font-bold">
+            Projects
+          </li>
+          <li className="p-4 border-b cursor-pointer hover:font-bold">
+            Skills
+          </li>
+          <li className="p-4 border-b cursor-pointer hover:font-bold">
+            Expierence
+          </li>
+          <li className="p-4 cursor-pointer hover:font-bold">Contact Me</li>
         </ul>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
